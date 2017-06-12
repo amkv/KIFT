f5605017c9f5d85bc6fc0297cc01d95f54c4102f
Artem Kalmykov <kalmykov.artem@gmail.com>
2017-06-12 12:14:33 -0700

    art

--- client.py
+++ client.py
@@ -3,13 +3,22 @@
 import requests
 import os
 import json
+import sys
 
-file = os.getcwd() + '/' + 'train/1_hello.wav'
-upload_url = 'http://127.0.0.1:4040/'
+def main(argv):
+    if len(argv) != 2:
+        print "usage: client <port>"
+        sys.exit(0)
+    file = os.getcwd() + '/' + 'train/1_hello.wav'
+    port = int(sys.argv[1])
+    upload_url = 'http://127.0.0.1' + str(port) + '/upload'
+    file_ = {'file': (file, open(file, 'rb'))}
+    try:
+        r = requests.post(upload_url, files=file_).text
+    except:
+        print "bad connection"
+        sys.exit(0)
+    print (r)
 
-file_ = {'file': (file, open(file, 'rb'))}
-r = requests.post(upload_url, files=file_).text
-
-# j = json.load(r)
-
-print (r)
+if __name__ == "__main__":
+    main(sys.argv)
--- server.py
+++ server.py
@@ -7,41 +7,53 @@ from werkzeug.utils import secure_filename
 import shutil
 import sys
 import os
-# from sqlalchemy import create_engine
 import json
+# from sqlalchemy import create_engine
 # from flask.ext.jsonpify import jsonify
 
+
+# Constant variables
 app = Flask(__name__)
 api = Api(app)
 PATH = os.getcwd() + '/'
+# Folder for upload incoming wav files
 UPLOAD_FOLDER = 'uploaded'
+OUTGOING_FOLDER = 'outgoing'
+# Checker for upload method, only wav
+# for example
+# ALLOWED_EXTENSIONS = set(['wav', 'mp3'])
 ALLOWED_EXTENSIONS = set(['wav'])
 app.config['UPLOAD_FOLDER'] = PATH + UPLOAD_FOLDER
 
 def set_folders():
+    """check the folders, delete if exist"""
     global PATH
     global UPLOAD_FOLDER
+    global OUTGOING_FOLDER
     if os.path.exists(PATH + UPLOAD_FOLDER):
         shutil.rmtree(PATH + UPLOAD_FOLDER)
+    if os.path.exists(PATH + OUTGOING_FOLDER):
+        shutil.rmtree(PATH + OUTGOING_FOLDER)
     os.mkdir(PATH + UPLOAD_FOLDER)
+    os.mkdir(PATH + OUTGOING_FOLDER)
 
 def print_usage():
+    """show usage and exit"""
     print ('usage: server.py <port number>')
     sys.exit(0)
 
 def set_port(argv):
+    """convert argument, check number in range, return port"""
     port = int(argv)
-    if port <= 100 or port >= 63000:
+    if port <= 100 or port >= 65535:
         print_usage()
     else:
         return port
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def handler(filename):
-
     response = json.dumps({'status': 'ok', 'wav': filename, 'link': 'http://google.com'}, sort_keys=True, indent=4)
     return response
 
@@ -53,8 +65,6 @@ def upload_file():
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
@@ -73,6 +83,7 @@ def upload_file():
     '''
 
 if __name__ == '__main__':
+    """main method."""
     if len(sys.argv) != 2:
         print_usage()
     set_folders()
