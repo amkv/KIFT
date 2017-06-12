30a4601ce451dbb692a6b276a6bcb0b8706424ce
Artem Kalmykov <kalmykov.artem@gmail.com>
2017-06-12 16:52:52 -0700

    turnof_servers

--- turnoff_server.py
+++ turnoff_server.py
@@ -2,13 +2,19 @@ import os
 import sys
 import subprocess
 
-output = subprocess.check_output('ps')
-text = output.split('\n')
+def main():
+    server = "src/server_src/server.py"
+    found = False
+    output = subprocess.check_output('ps')
+    text = output.split('\n')
+    for line in text:
+        if server in line:
+            pid = line.split(' ')[1]
+            os.system('kill %(pid)s' % {'pid': pid})
+            print('process %(pid)s killed' % {'pid': pid})
+            found = True
+    if not found:
+        print('no active servers')
 
-for line in text:
-    if "src/server_src/server.py" in line:
-        pid = line.split(' ')[1]
-        os.system('kill %(pid)s' % {'pid': pid})
-        print('process %(pid)s killed' % {'pid': pid})
-        sys.exit(0)
-print('no active servers')
+if __name__ == "__main__":
+    main()
